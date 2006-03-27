ce3c4a1c137e5e374da05b265bd577693256f4f1
John Bray <bray@ucar.edu>
2006-03-27 20:01:19 +0000

    Last vestiges of obsolete D3VAR_IRY_KLUDGE removed
    git-svn-id: https://svn-wrf-var.cgd.ucar.edu/trunk@551 70815b0f-b814-0410-81f7-dfa10a885da6

--- tools/registry.c
+++ tools/registry.c
@@ -20,7 +20,6 @@ main( int argc, char *argv[], char *env[] )
 
   sw_deref_kludge           = 0 ;
   sw_io_deref_kludge        = 0 ;
-  sw_3dvar_iry_kludge           = 0 ;
   sw_distrib_io_layer       = 1 ;
   sw_limit_args             = 0 ; /* usually set -- except for GRAPS */
   sw_dm_parallel            = 0 ;
@@ -51,13 +50,6 @@ main( int argc, char *argv[], char *env[] )
       if (!strcmp(*argv,"-DIFORT_KLUDGE")) {
         sw_ifort_kludge = 1 ;
       }
-      if (!strcmp(*argv,"-DD3VAR_IRY_KLUDGE")) {
-#if 0
-        sw_3dvar_iry_kludge = 1 ;
-#else
-        fprintf(stderr,"WARNING: -DD3VAR_IRY_KLUDGE option obsolete (it is now disabled by default). Ignored.\n") ;
-#endif
-      }
       if (!strcmp(*argv,"-DALL_X_STAGGERED")) {
         sw_all_x_staggered = 1 ;
       }
@@ -78,7 +70,7 @@ main( int argc, char *argv[], char *env[] )
         sw_dm_serial_in_only = 1 ;
       }
       if (!strncmp(*argv,"-h",2)) {
-        fprintf(stderr,"Usage: %s [-DDEREF_KLUDGE] [-DDM_PARALLEL] [-DDISTRIB_IO_LAYER] [-DDM_SERIAL_IN_ONLY] [-DD3VAR_IRY_KLUDGE] registryfile\n",thisprog) ;
+        fprintf(stderr,"Usage: %s [-DDEREF_KLUDGE] [-DDM_PARALLEL] [-DDISTRIB_IO_LAYER] [-DDM_SERIAL_IN_ONLY] registryfile\n",thisprog) ;
         exit(1) ;
       }
     }
