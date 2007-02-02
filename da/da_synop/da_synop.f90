762221d37b62b887df593c62bc714b288e18940d
John Bray <bray@ucar.edu>
2007-02-02 00:11:23 +0000

    Fix compliation erros introduced today
    git-svn-id: https://svn-wrf-var.cgd.ucar.edu/trunk@2116 70815b0f-b814-0410-81f7-dfa10a885da6

--- da/da_synop/da_synop.f90
+++ da/da_synop/da_synop.f90
@@ -4,7 +4,7 @@ module da_synop
       check_max_iv_print, check_max_iv_unit, v_interp_p, v_interp_h, &
       check_max_iv, missing, max_error_uv, max_error_t, rootproc, &
       num_synop_tot, max_error_p,max_error_q, sfc_assi_options, &
-      max_stheight_diff,testing_dm_exact
+      max_stheight_diff,testing_dm_exact, anal_type_verify
    use da_define_structures, only : maxmin_type, ob_type, y_type, jo_type, &
       bad_data_type, x_type, synop_type, number_type, bad_data_type
    use module_domain, only : xpose_type, xb_type
