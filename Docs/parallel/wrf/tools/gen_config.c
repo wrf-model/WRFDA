2848f5bf20f278c5318131c7c5bfbeb135bd819a
John Bray <bray@ucar.edu>
2007-05-17 23:17:15 +0000

    Changes to match wrfvar changes, affecting wrfnl patch logic and beadspreads
    1) Quilting namelist can be changed by defining NL_NIO_TASKS_PER_GROUP
    and NL_NIO_GROUPS. No effect on wrfvar, but might speed up WRF.
    
    2) namelist.output explicitly opened, rather than using fort.9
    
    
    M    Docs/parallel/wrf/tools/gen_config.c
    M    Docs/parallel/wrf/frame/module_configure.F
    
    
    git-svn-id: https://svn-wrf-var.cgd.ucar.edu/branches/BR_2005workshop@2633 70815b0f-b814-0410-81f7-dfa10a885da6

--- Docs/parallel/wrf/tools/gen_config.c
+++ Docs/parallel/wrf/tools/gen_config.c
@@ -133,6 +133,7 @@ gen_namelist_script ( char * dirname )
   sym_forget() ;
 
   fprintf(fp,"# Machine generated, do not edit\n\n") ;
+  fprintf(fp,"FILE=${1:-namelist.input}\n\n");
 
   for ( p = Domain.fields ; p != NULL ; p = p-> next )
   {
@@ -148,8 +149,6 @@ gen_namelist_script ( char * dirname )
           continue ;
         }
 	if (sym_get( p2 ) == NULL) { /* not in table yet */
-          
-          fprintf(fp,"FILE=${1:-namelist.input}\n\n");
           fprintf(fp,"echo \\&%s >> $FILE\n",p2) ;
           for ( q = Domain.fields ; q != NULL ; q = q-> next ) {
             if ( q->node_kind & RCONFIG) {
@@ -192,6 +191,8 @@ gen_namelist_script ( char * dirname )
   }
   
   fprintf(fp,"echo \\&namelist_quilt >> $FILE\n");
+  fprintf(fp,"if test ! -z \"$NL_NIO_TASKS_PER_GROUP\"; then echo \"nio_tasks_per_group=${NL_NIO_TASKS_PER_GROUP},\" >> $FILE;fi\n");
+  fprintf(fp,"if test ! -z \"$NL_NIO_GROUPS\"; then echo \"nio_groups=${NL_NIO_GROUPS},\" >> $FILE;fi\n");
   fprintf(fp,"echo / >> $FILE\n");
 
   fclose( fp ) ;
