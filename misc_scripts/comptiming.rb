7b550bdaa1783629a11e77b578e4149b6e645ee7
Tom Henderson <Thomas.B.Henderson@noaa.gov>
2007-06-04 16:10:36 +0000

    Tested OK with Xin's new WPS-generated input data set from blueice:/ptmp/xinzhang/case_data/cwb/135km/rc/.
    Note that the complete input data set is stored on MSS along with the new 
    parallel baseline:  
    
    hender ON bl0312en IN /ptmp/hender/4DVAR_Optimization >> msrcp -pe 3650 4DVAR_case_data_Xin_WPS.tar.gz mss:4DVAR_case_data_Xin_WPS.tar.gz
    hender ON bl0312en IN /ptmp/hender/4DVAR_Optimization >> msls -l | grep Xin_WPS
    -rw----rw-  1 HENDER  64000400    264272165 Jun  4 09:50 4DVAR_case_data_Xin_WPS.tar.gz
    -rw----rw-  1 HENDER  64000400    127984347 Jun  4 09:49 cwb_BASELINE_Xin_WPS.tar.gz
    
    Results for 1-task and 16-task MPI runs are bitwise-exact for the adjoint run 
    in isolation (except for the usual 4 bytes of patch meta-data).  
    
    
    
    git-svn-id: https://svn-wrf-var.cgd.ucar.edu/branches/BR_2005_OPT@2681 70815b0f-b814-0410-81f7-dfa10a885da6

--- misc_scripts/comptiming.rb
+++ misc_scripts/comptiming.rb
@@ -16,13 +16,13 @@
 # * Writing an ASCII report.  
 #
 
-class CompTiming
+class DBENCHTiming
 
-  # CompTiming.new( aString )
+  # DBENCHTiming.new( aString )
   # Argument is file name.  
   def initialize( fname )
     @filename = fname
-    @comp_timing_results = Hash.new
+    @compexch_timing_results = Hash.new
     @solve_tim = 0
     parse_timing
     print_report
@@ -34,8 +34,13 @@ class CompTiming
     lines.each do |line|
       if (line =~ /comp(\w*)\s*=\s*/) then
         new_key = "comp" + $1.dup
-        @comp_timing_results[new_key] = 0 unless (@comp_timing_results.has_key?(new_key))
-        @comp_timing_results[new_key] += $'.to_i
+        @compexch_timing_results[new_key] = 0 unless (@compexch_timing_results.has_key?(new_key))
+        @compexch_timing_results[new_key] += $'.to_i
+      elsif (line =~ /exch(\w*)\s*=\s*/) then
+        # refactor to remove duplication
+        new_key = "exch" + $1.dup
+        @compexch_timing_results[new_key] = 0 unless (@compexch_timing_results.has_key?(new_key))
+        @compexch_timing_results[new_key] += $'.to_i
       elsif (line =~ /solve_tim\s*=\s*/) then
         @solve_tim += $'.to_i
       end
@@ -44,8 +49,14 @@ class CompTiming
 
   def print_report
     total_time = 0
-    @comp_timing_results.each_value { |val| total_time += val }
-    sorted_results = @comp_timing_results.sort {|a,b| b[1]<=>a[1]}
+    total_comptime = 0
+    total_exchtime = 0
+    @compexch_timing_results.each do |key,val|
+      total_time += val
+      total_comptime += val if (key =~ /^comp/)
+      total_exchtime += val if (key =~ /^exch/)
+    end
+    sorted_results = @compexch_timing_results.sort {|a,b| b[1]<=>a[1]}
     sorted_results.each do |timer|
       fraction = timer[1].to_f / total_time.to_f
       # TODO:  use a better way to format printed Float 
@@ -53,16 +64,18 @@ class CompTiming
       percent = ifraction.to_f / 100.0
       puts "Timer #{timer[0]}:  #{timer[1]}  #{percent}"
     end
-    puts "Sum of all \"comp\" times = #{total_time}"
+    puts "Sum of all \"comp\" times = #{total_comptime}"
+    puts "Sum of all \"exch\" times = #{total_exchtime}"
+    puts "Sum of all \"comp\" and \"exch\" times = #{total_time}"
     puts "Sum of all \"solve_tim\" times = #{@solve_tim}"
   end
 
-end  # class CompTiming
+end  # class DBENCHTiming
 
 
 if $0 == __FILE__
 
-CompTiming.new(ARGV[0])
+DBENCHTiming.new(ARGV[0])
 
 end
 
