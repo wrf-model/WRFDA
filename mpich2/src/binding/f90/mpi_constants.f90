da128f5daa6b50d99d1afeee050dafcb1a74e614
John Bray <bray@ucar.edu>
2007-10-31 17:19:43 +0000

    Damn, wrong place
    git-svn-id: https://svn-wrf-var.cgd.ucar.edu/vendor@3215 70815b0f-b814-0410-81f7-dfa10a885da6

--- mpich2/src/binding/f90/mpi_constants.f90
+++
@@ -1,7 +0,0 @@
-! -*- Mode:F90; -*-
-!     (C) 2004 by Argonne National Laboratory.
-!     See COPYRIGHT in top-level directory.
-!
-        MODULE MPI_CONSTANTS
-        INCLUDE 'mpifnoext.h'
-        END MODULE MPI_CONSTANTS
