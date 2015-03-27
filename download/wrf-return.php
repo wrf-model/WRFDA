29cb91a5cfad08e49608357d072a024c66ed7f51
Michael Kavulich <kavulich@ucar.edu>
2015-03-27 00:42:25 +0000

    WEB
    Regression page: Including compiler version numbers in js files in case of later multi-version tests
    M    regression/index.html
    
    Publications page: Adding a ton more from the past year; this is just from those citing the 3dvar and 4dvar papers
                       Some formatting changes
    M    publications.html
    
    CSS Stylesheet: making margins a little smaller
    M    images/wrfstyle.css
    
    WRF Registration/download pages: updated to new look (well overdue)
    M    download/wrf-regist.php
    M    download/wrf-return.php
    
    
    
    git-svn-id: https://svn-user-kavulich.cgd.ucar.edu/trunk/WEB@140 95a97b06-1a33-401f-800d-6477af4236fe

--- download/wrf-return.php
+++ download/wrf-return.php
@@ -94,20 +94,20 @@ $(document).ready(function() {
 
 </head>
 
-<body bgcolor="#FFFFFF" background="../images/back2.jpg" link="#000099" vlink="#000099" alink="#FF3300" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<body bgcolor="#FFFFFF" link="#000099" vlink="#000099" alink="#FF3300" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
   <tr bgcolor="#000000">
     <td height="10" bgcolor="#000000"><div align="left"><img src="images/transgif.gif" width="1" height="1"></div>
     </td>
   </tr>
   <tr valign="top">
-    <td height="10" bgcolor="#FFFFFF"><img src="../images/header.jpg" width="566" height="56"></td>
+    <td height="10" bgcolor="#DDFFDD"><img src="../images/header_new_cropped.png" height="56" border="0"></td>
   </tr>
   <tr bgcolor="#FFCC33">
-    <td height="6" background="../images/goldrule.jpg"><img src="../images/goldrule.jpg" width="1" height="1"></td>
+    <td height="6" background="../images/goldrule.jpg" bgcolor="#FFCC33"><img src="../images/goldrule.jpg" width="1" height="1"></td>
   </tr>
   <tr>
-    <td height="22" bgcolor="#009933">&nbsp;</td>
+    <td height="34" bgcolor="#009933">&nbsp;</td>
   </tr>
   <tr>
     <td height="2" bgcolor="#ffcc66"><img src="../images/transgif.gif" width="1" height="1"></td>
@@ -115,20 +115,24 @@ $(document).ready(function() {
 </table>
 <table width="100%" height="89%" border="0" cellpadding="0" cellspacing="0" class="background">
   <tr>
-    <td height="865" valign="top" bgcolor="#CBFFCD"><table width="780" height="868" border="0" cellpadding="0" cellspacing="0">
+    <td width="100%" height="859" valign="top" bgcolor="#CBFFCD"><table style=max-width:1000px; border="0" cellpadding="0" cellspacing="0">
       <tr valign="top">
-        <td width="117" height="868" bgcolor="#009933"><p>&nbsp;</p>
-          <table width="100" border="0" align="center" cellpadding="0" cellspacing="0">
-            <tr>
-              <td><p align="right"><a href="get_source.html" class="leftnavlink">WRFDA</a></p>
-                  <p align="right"><a href="free_data.html" class="leftnavlink">Free 
-                    Met Data from NCAR</a></p>
-                  <p align="right"><a href="ftp://ftpprd.ncep.noaa.gov/pub/data/nccf/com" class="leftnavlink">Real-time 
-                    Data from NCEP</a></p></td>
-            </tr>
-          </table>          
-          <p>&nbsp;</p>          <p>&nbsp;</p></td>
-          <td><p>&nbsp;</p>
+        <td width="117" height="910" bgcolor="#009933"><p>&nbsp;</p>
+
+          <table width="117" border="0" align="center" cellpadding="0" cellspacing="0">
+              <td><p align="center"><a href="../downloads.html" class="leftnavlink">Overview</a></p>
+                <p align="center"><a href="get_source.html" class="leftnavlink">WRFDA</a></p>
+                <p align="center"><a href="wrfplus.html" class="leftnavlink">WRFPLUS</a></p>
+                <p align="center"><a href="testdata.html" class="leftnavlink">Test data</a></p>
+                <p align="center"><a href="fso.html" class="leftnavlink">FSO</a></p>
+                <p align="center"><a href="madis.html" class="leftnavlink">MADIS2LITTLER</a></p>
+                <p align="center"><a href="tools.html" class="leftnavlink">Tools</a></p>
+                <p align="center"><a href="free_data.html" class="leftnavlink">Free Input Data</a></p>
+              </td>
+          </table>
+
+          <p>&nbsp;</p>          <p>&nbsp;</p>          <img src="../images/shim112.gif" width="112" height="1"></td>
+          <td bgcolor="#CBFFCD"><p>&nbsp;</p>
             <p class="pagetitle">WRFDA Source Code Download: Returning Users</p>
             <p class="bodytext">
 	    <!-- ################################################################ -->
